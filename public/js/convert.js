6f7a2e65681e81cee90fa94eae64f4c71ff859a9
artemij-petrovskij <petrovsky.artem.dev@gmail.com>
2020-06-12 12:53:00 +0300

    add PLN + numeric input mode

--- public/js/convert.js
+++ public/js/convert.js
@@ -8,16 +8,18 @@ function ifNotNum(e) {
    return e.target.value = e.target.value.replace(/[^\d,]/g, '');
 }
 
-function convert(USD, EUR, RUB) {
+function convert(USD, EUR, RUB, PLN) {
 
     const usd = USD
     const eur = EUR
     const rub = RUB
+    const pln = PLN
 
     let inputBLR = document.querySelector('#blr')
     let inputUSD = document.querySelector('#usd')
     let inputEUR = document.querySelector('#eur')
     let inputRUB = document.querySelector('#rub')
+    let inputPLN = document.querySelector('#pln')
 
 
     inputBLR.addEventListener('input', (e) => {
@@ -25,6 +27,7 @@ function convert(USD, EUR, RUB) {
         inputUSD.value = formatNumber(e.target.value / usd)
         inputEUR.value = formatNumber(e.target.value / eur)
         inputRUB.value = formatNumber(e.target.value / rub * 100)
+        inputPLN.value = formatNumber(e.target.value / pln * 10)
     })
 
     inputUSD.addEventListener('input', (e) => {
@@ -32,6 +35,8 @@ function convert(USD, EUR, RUB) {
         inputBLR.value = formatNumber(e.target.value * usd)
         inputEUR.value = formatNumber(usd / eur * e.target.value)
         inputRUB.value = formatNumber(usd / rub * e.target.value * 100)
+        inputPLN.value = formatNumber(usd / pln * e.target.value * 10)
+
     })
 
     inputEUR.addEventListener('input', (e) => {
@@ -39,11 +44,24 @@ function convert(USD, EUR, RUB) {
         inputBLR.value = formatNumber(e.target.value * eur)
         inputUSD.value = formatNumber(eur / usd * e.target.value)
         inputRUB.value = formatNumber(eur / rub * e.target.value * 100)
+        inputPLN.value = formatNumber(eur / pln * e.target.value * 10)
+
     })
+
     inputRUB.addEventListener('input', (e) => {
         ifNotNum(e)
         inputBLR.value = formatNumber(e.target.value * rub / 100)
         inputUSD.value = formatNumber(rub / usd * e.target.value / 100)
         inputEUR.value = formatNumber(rub / eur * e.target.value / 100)
+        inputPLN.value = formatNumber(rub / pln * e.target.value / 100 *10)
+
+    })
+
+    inputPLN.addEventListener('input', (e) => {
+        ifNotNum(e)
+        inputBLR.value = formatNumber(e.target.value * pln / 10)
+        inputUSD.value = formatNumber(pln / usd * e.target.value / 10)
+        inputEUR.value = formatNumber(pln / eur * e.target.value / 10)
+        inputRUB.value = formatNumber(pln / rub * e.target.value * 10)
     })
 }
--- router/main_route.js
+++ router/main_route.js
@@ -9,7 +9,8 @@ router.get('/', async function (req, res) {
             title:'Курсы валют',
             USD: data[4].Cur_OfficialRate,
             EUR: data[5].Cur_OfficialRate,
-            RUB: data[16].Cur_OfficialRate
+            RUB: data[16].Cur_OfficialRate,
+            PLN: data[6].Cur_OfficialRate
         });
     }catch(err){
         res.redirect('/err')
